44a09159bbd347f869d7d00e0355da3b34d61c5a
Stuart Creed <stuart.a.creed@gmail.com>
2019-12-08 17:07:41 +0000

    Initial Commit

---
+++ confusion/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Love a burt</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'aqua',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
