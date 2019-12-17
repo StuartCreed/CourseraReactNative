fdede6d0fccebc603849c17cd78d75fa1ded7ee5
Stuart Creed <stuart.a.creed@gmail.com>
2019-12-17 21:43:27 +0000

    Revert "Revert "Using Redux""
    This reverts commit 79b517f20e93fc34c71ce5b2abefadd587ea5a75.

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -8,6 +8,24 @@ import { DISHES } from '../shared/dishes';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import ContactUs from './ContactComponent';
+import { connect } from 'react-redux';
+import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchDishes: () => dispatch(fetchDishes()),
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders()),
+})
 
 const MenuNavigator = createStackNavigator({
       Menu: { screen: Menu,
@@ -168,6 +186,13 @@ const MainNavigator = createDrawerNavigator({
 
 class Main extends Component {
 
+   componentDidMount() {
+       this.props.fetchDishes();
+       this.props.fetchComments();
+       this.props.fetchPromos();
+       this.props.fetchLeaders();
+   }
+
   render() {
 
     return (
@@ -202,4 +227,4 @@ const styles = StyleSheet.create({
      }
 });
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
