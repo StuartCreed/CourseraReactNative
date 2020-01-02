916e1fad1a31155c39d35fc5e44b2fdabf2cfe9b
Stuart Creed <stuart.a.creed@gmail.com>
2020-01-02 22:16:46 +0000

    Favorites

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -11,6 +11,7 @@ import ContactUs from './ContactComponent';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 
 const mapStateToProps = state => {
   return {
@@ -51,6 +52,23 @@ const MenuNavigator = createStackNavigator({
       }
    }
 );
+
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }}
+      onPress={ () => navigation.navigate('DrawerToggle') } />
+  })
+})
 
 const ReservationNavigator = createStackNavigator({
   Reservation: { screen: Reservation }
@@ -197,25 +215,40 @@ const MainNavigator = createDrawerNavigator({
              ),
            }
          },
-   Reservation:
-       { screen: ReservationNavigator,
-         navigationOptions: {
-           title: 'Reserve Table',
-           drawerLabel: 'Reserve Table',
-           drawerIcon: ({ tintColor, focused }) => (
-             <Icon
-               name='cutlery'
-               type='font-awesome'
-               size={24}
-               iconStyle={{ color: tintColor }}
-             />
-           ),
+      Favorites:
+            { screen: FavoritesNavigator,
+               navigationOptions: {
+                 title: 'My Favorites',
+                 drawerLabel: 'My Favorites',
+                 drawerIcon: ({ tintColor, focused }) => (
+                   <Icon
+                     name='heart'
+                     type='font-awesome'
+                     size={24}
+                     iconStyle={{ color: tintColor }}
+                   />
+                 ),
+               }
+            },
+      Reservation:
+          { screen: ReservationNavigator,
+            navigationOptions: {
+              title: 'Reserve Table',
+              drawerLabel: 'Reserve Table',
+              drawerIcon: ({ tintColor, focused }) => (
+                <Icon
+                  name='cutlery'
+                  type='font-awesome'
+                  size={24}
+                  iconStyle={{ color: tintColor }}
+                />
+              ),
+            }
          }
-      }
-}, {
-  drawerBackgroundColor: '#D1C4E9',
-  contentComponent: CustomDrawerContentComponent
-});
+   }, {
+     drawerBackgroundColor: '#D1C4E9',
+     contentComponent: CustomDrawerContentComponent
+   });
 
 class Main extends Component {
 
