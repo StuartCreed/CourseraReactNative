270ffa672ed64141515755a8884f25b2711f09fc
StuartCreed <stuart.a.creed@gmail.com>
2020-01-09 08:25:03 +0000

    Local Notifications

--- confusion/components/LoginComponent.js
+++ confusion/components/LoginComponent.js
@@ -44,34 +44,38 @@ class Login extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <Input
-                    placeholder="Username"
-                    leftIcon={{ type: 'font-awesome', name: 'user-o' }}
-                    onChangeText={(username) => this.setState({username})}
-                    value={this.state.username}
-                    containerStyle={styles.formInput}
-                    />
-                <Input
-                    placeholder="Password"
-                    leftIcon={{ type: 'font-awesome', name: 'key' }}
-                    onChangeText={(password) => this.setState({password})}
-                    value={this.state.password}
-                    containerStyle={styles.formInput}
-                    />
-                <CheckBox title="Remember Me"
-                    center
-                    checked={this.state.remember}
-                    onPress={() => this.setState({remember: !this.state.remember})}
-                    containerStyle={styles.formCheckbox}
-                    />
-                <View style={styles.formButton}>
-                    <Button
-                        onPress={() => this.handleLogin()}
-                        title="Login"
-                        color="#512DA8"
-                        />
-                </View>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+              <View style={{ width: '100%', justifyContent: 'center'}}>
+                  <Input
+                      placeholder=" Username"
+                      leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                      onChangeText={(username) => this.setState({username})}
+                      value={this.state.username}
+                      containerStyle={styles.formInput}
+                      leftIconContainerStyle={{margin: 10}}
+                      />
+                  <Input
+                      placeholder=" Password"
+                      leftIcon={{ type: 'font-awesome', name: 'key' }}
+                      onChangeText={(password) => this.setState({password})}
+                      value={this.state.password}
+                      containerStyle={styles.formInput}
+                      leftIconContainerStyle={{margin: 10}}
+                      />
+                  <CheckBox title="Remember Me"
+                      center
+                      checked={this.state.remember}
+                      onPress={() => this.setState({remember: !this.state.remember})}
+                      containerStyle={styles.formCheckbox}
+                      />
+                  <View style={styles.formButton}>
+                      <Button
+                          onPress={() => this.handleLogin()}
+                          title="Login"
+                          color="#512DA8"
+                          />
+                  </View>
+              </View>
             </View>
         );
     }
@@ -84,7 +88,7 @@ const styles = StyleSheet.create({
         margin: 20,
     },
     formInput: {
-        margin: 40
+        marginRight: 40
     },
     formCheckbox: {
         margin: 40,
--- confusion/shared/BaseUrl.js
+++ confusion/shared/BaseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://192.168.1.88:3001/";
+export const baseUrl = "http://192.168.1.98:3001/";
