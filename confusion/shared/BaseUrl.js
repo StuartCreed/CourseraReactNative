63f565616bbb5313c63ded6778641b55514c6104
StuartCreed <stuart.a.creed@gmail.com>
2020-01-15 10:13:55 +0000

    Final Commint of React Native Learning Project

--- confusion/shared/BaseUrl.js
+++ confusion/shared/BaseUrl.js
@@ -1,2 +1,20 @@
-export const baseUrl = "http://192.168.1.98:3001/";
-/*export const baseUrl = "../shared/";*/
+import { serverOffline } from '../shared/serverOffline';
+/*serverOfflineMode is off at the moment as I could not get the below function to work yet so it can work from any location. Either that or I put it on a proper server*/
+
+/*offlineCheck(serverOffline){
+  if (serverOffline == true) {
+    console.log("Server Offline Mode Selected");
+    const baseUrlStatus = "";
+  };
+
+  else {
+    console.log("Server Offline Mode Selected");
+    const baseUrlStatus = "http://192.168.1.98:3001/";
+  }
+  return(baseUrlStatus)
+};
+
+() => this.offlineCheck()
+*/
+
+export const baseUrl ="http://192.168.1.98:3001/";
---
+++ confusion/shared/serverOffline.js
@@ -0,0 +1,1 @@
+export const serverOffline = false;
